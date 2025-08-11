5833686230921eb458552290887650ece647ac43
EC2 Default User <ec2-user@ip-172-31-36-49.ap-northeast-1.compute.internal>
2025-08-11 23:51:41 +0000

    public/top,aboutページ追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
+  root to: 'public/homes#top'
+  get '/about', to: 'public/homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
