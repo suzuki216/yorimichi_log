6264656864f6970a4e585520481dd04a843acd35
EC2 Default User <ec2-user@ip-172-31-36-49.ap-northeast-1.compute.internal>
2025-08-14 14:45:40 +0000

    ログイン関係修正中１

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,35 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'public/users/registrations',
+    sessions: 'public/users/sessions'
+  },
+  path: 'users', path_names: {
+    sign_in: 'sign_in',
+    sign_out: 'sign_out',
+    sign_up: 'sign_up'
+  }
+
+  devise_for :admins, controllers: {
+    sessions: 'admin/sessions'
+  },
+  path: 'admin', path_names: {
+    sign_in: 'sign_in',
+    sign_out: 'sign_out'
+  }
+  
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'public/users/sessions#guest_sign_in'
+  end
+
   namespace :public do
     get 'homes/top'
     get 'homes/about'
   end
+
+  namespace :admin do
+    root to: 'homes#top'
+  end
+
   root to: 'public/homes#top'
   get '/about', to: 'public/homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
